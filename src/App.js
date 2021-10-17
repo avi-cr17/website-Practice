6d4d6cafbeb1731cf4ef8af563b76bbfe4aa9b73
Avinandan <avinandan.agt@gmail.com>
2021-10-17 20:22:43 +0530

    scroll adjusted navbar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import About from './components/About';
 import Project from './components/Project';
+import {useEffect, useState} from 'react';
 
 
 
 function App() {
+
+  const [NavBarClass,setClass]= useState('navBig');
+
+  const onScroll = ()=>{
+    if(window.scrollY==0)
+    setClass('navSmall');
+    if(window.scrollY<=10)
+    setClass('navBig');
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+  },[]);
+
   return (
     <>
-    <div className='nave'>
+    <div className={NavBarClass}>
     <Navbar />
     </div>
     <Switch>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  
+
     return (
         <>
-        <div className="container-fluid nav_bg " style={{ backgroundColor:'lightblue'}}>
+        <div className="container-fluid nav_bg " >
             <div className="row" >
-                <div className="col-10 mx-auto">
+                <div className="d-flex justify-content-between align-items-center col-10 mx-auto">
 
 
               
-    <nav className="navbar navbar-expand-lg navbar-light ">
+  {/*}  <nav className="navbar navbar-expand-lg navbar-light ">
   <div className="container-fluid ">
     <NavLink to="/" className="nav-link onhover" >Home{'<3'}  </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +30,28 @@ const Navbar = () => {
       </ul>
     </div>
   </div>
+  
 </nav>  
+    */}
+    <nav className="navbar navbar-expand-lg navbar-light  w-100">
+      <div className=' col-4 navbar-nav'>
+      <NavLink  to="/" className="nav-link onhover" >Home{'<3'}  </NavLink>
+      </div>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse " id="navbarNav">
+      <div className="navbar-nav text-center d-flex justify-content-end w-100 ">
+        <div className="nav-item mx-3">
+          <NavLink  to="/project"  className="nav-link onhover" aria-current="page" >Projects</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink  to="/about" className="nav-link onhover">About Me</NavLink>
+        </div>   
+      </div>
+    </div>
+    </nav>
+    
 
                 </div>
             </div>
