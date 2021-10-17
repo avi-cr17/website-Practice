2d5fd352d5f84e2c0af5951d89071534a3b8930b
Avinandan <avinandan.agt@gmail.com>
2021-10-17 18:03:37 +0530

    nav bar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,17 +12,17 @@ const Navbar = () => {
               
     <nav className="navbar navbar-expand-lg navbar-light ">
   <div className="container-fluid ">
-    <NavLink to="/" className="navbar-brand onhover" ><strong>Home{'<3'} </strong> </NavLink>
+    <NavLink to="/" className="nav-link onhover" >Home{'<3'}  </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse  offset-md-8" id="navbarNav">
+    <div className="collapse navbar-collapse  offset-md-7" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <NavLink  to="/project"  className="nav-link active onhover" aria-current="page" ><strong>Projects</strong></NavLink>
+          <NavLink  to="/project"  className="nav-link onhover" aria-current="page" >Projects</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink  to="/about" className="nav-link onhover"><strong>About Me</strong></NavLink>
+          <NavLink  to="/about" className="nav-link onhover">About Me</NavLink>
         </li>   
       </ul>
     </div>
